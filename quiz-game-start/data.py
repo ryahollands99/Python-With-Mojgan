6e2697792850ca038553b0e82518ecb192ab0b97
ryahollands99 <44526328+ryahollands99@users.noreply.github.com>
2022-09-25 14:58:40 -0700

    Add files via upload
    Quiz game that asks the user a trivia question and keeps score.

---
+++ quiz-game-start/data.py
@@ -0,0 +1,36 @@
+question_data = [
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "In World War II, Hawker Typhoons served in the Pacific theater.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a "
+                 "quote from French philosopher Voltaire.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "Brezhnev was the 5th leader of the USSR.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to "
+                 "raise moral questions about banning drugs.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "Abraham Lincoln was the first U.S. President to be born outside the borders of the thirteen original "
+                 "states. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "United States President Ronald Reagan was the first president to appoint a woman to the "
+                 "Supreme Court. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "The Hundred Years&#039; War was fought for more than a hundred years.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "Martin Luther King Jr. and Anne Frank were born the same year. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+     "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
+]
